958c3ad4d77ac419abacec38472dd66be51192ff
Ivette Flores <floresivette@berkeley.edu>
2015-09-25 18:24:55 -0700

    Completed HW (Had issues installing ruby)

--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,18 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  sum = a + b
+  sum * sum
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  sorted_array = a.sort
+  sorted_array.map {|n| n + 1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
